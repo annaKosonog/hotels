d7d2e68f458a15c8004da068de6b9b6d73894853
annaKosonog <anna.kosonog21@gmail.com>
2021-07-14 22:37:40 +0200

    "Exceptions handler class "

--- src/main/java/akademia/exception/ResourceNotFoundException.java
+++ src/main/java/akademia/exception/ResourceNotFoundException.java
@@ -1,15 +1,17 @@
 package akademia.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import lombok.Getter;
 
 import javax.persistence.EntityNotFoundException;
 
-@ResponseStatus(HttpStatus.NOT_FOUND) // oznacza
+@Getter
 public class ResourceNotFoundException extends EntityNotFoundException {
 
+    private static final long serialVersionUID = -4856846361193249489L;
+    private final String infoRequestPartnerCode;
 
-    public ResourceNotFoundException(String message) {
-        super(message);
+    public ResourceNotFoundException(String partnerCode) {
+        super(String.format("Hotel not found about given partner_code: ", partnerCode));
+        this.infoRequestPartnerCode = partnerCode;
     }
 }
